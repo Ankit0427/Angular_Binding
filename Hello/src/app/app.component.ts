27003d34d87e3cd381587bf452458c7e65917799
Ankit0427 <153299876+Ankit0427@users.noreply.github.com>
2025-03-07 10:52:50 +0530

    [Ankit kumar] Initial commit

---
+++ Hello/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hello From BridgeLabz';
+
+  ngOnInit():void{
+    this.title="Hello From BridgeLabz.";
+  }
+}
