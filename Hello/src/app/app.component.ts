c82cc5997751287e341b048ec7ea44a17b52d8c6
Ankit0427 <153299876+Ankit0427@users.noreply.github.com>
2025-03-07 13:15:57 +0530

    [Ankit Kumar] Opening on new tab

--- Hello/src/app/app.component.ts
+++ Hello/src/app/app.component.ts
@@ -7,8 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello From BridgeLabz';
+  imgUrl="../assets/bridgelabz_com_logo.jpeg";
+  url="https://www.bridgelabz.com";
 
   ngOnInit():void{
     this.title="Hello From BridgeLabz.";
+
+  }
+
+  onClick($event:Event)
+  {
+    console.log("Save Button is Clicked", $event);
+    window.open(this.url,"_blank");
   }
 }
