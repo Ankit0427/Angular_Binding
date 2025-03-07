b89e0458956726a144ecc4925c153904c14b7b88
Ankit0427 <153299876+Ankit0427@users.noreply.github.com>
2025-03-07 13:22:35 +0530

    [Ankit kumar] Adding Input Display file

---
+++ HelloWorld/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'HelloWorld';
+  imgUrl="../assets/images.jpg";
+  url="https://www.bridgelabz.com/"
+  userName: string = "";
+
+  ngOnInit():void{
+    this.title = "Hello from BridgeLabz.";
+  }
+
+  onClick($event:Event){
+    console.log("Save button is clicked", $event);
+    window.open(this.url, "_blank");
+  }
+}
